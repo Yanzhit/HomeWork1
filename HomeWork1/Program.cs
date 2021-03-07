ffd38d029220252b3b07ea99c7b7014db86dac02
Yanzhit <yanzhit@gmail.com>
2021-03-07 18:58:54 +0300

    Initial

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Переменные
+            double a = 2, b = 5, c=2, x1=2, y1=5, x2=2, y2=3;
+            string s1 = "123", s2 = "456";
+            
+            Console.WriteLine("1. " + Variables.SolutionEquation(a, b));
+            var s3 = Variables.ToSwap(s1, s2);
+            Console.WriteLine($"2. {s3.Item1} {s3.Item2}");
+            Console.WriteLine("3. " + Variables.Division(a, b));
+            Console.WriteLine("4. " + Variables.SolutionLinearEquation(a, b, c));
+            var ab = Variables.SolutionEquationСoordinates(x1, y1, x2, y2);
+            Console.WriteLine($"5. {ab.Item1} {ab.Item2} ");
+
+            //Ветвление
+
+
+        }
+    }
+}
---
+++ HomeWork1/Variables.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork1
+{
+    public static class Variables
+    {
+        // Решение уравнения (5 * a + b * b) / (b - a)
+
+        public static double SolutionEquation(double a, double b)
+        {
+            if (a==b)
+            {
+                throw new Exception("Не возможно решить уравнение т.к. B-A = 0 ");                
+            }
+            else
+            {
+                return (5 * a + b * b) / (b - a);
+            }
+        }
+
+        // Поменять местами А и В
+
+        public static (string, string) ToSwap(string a, string b)
+        {
+            string tmp;
+            tmp = a;
+            a = b;
+            b = tmp;
+            return (a, b);
+        }
+
+        // Деление А на В
+
+        public static double Division(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new Exception("Не возможно решить уравнение т.к. b = 0 ");
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+
+        // Решение линейного уравнения стандартного вида
+
+        public static double SolutionLinearEquation(double a, double b, double c)
+        {
+            if (a == 0)
+            {
+                throw new Exception("Не возможно решить уравнение т.к. a = 0 ");
+            }
+            else
+            {
+                return (c - b) / a;
+            }
+        }
+
+        // Решение уравнения по кооординатам
+        
+        public static (double, double) SolutionEquationСoordinates(double x1, double y1, double x2, double y2)
+        {
+            if (x1 == x2)
+            {
+                throw new Exception("Не возможно решить уравнение т.к. x1 - x2 = 0");
+            }
+            else
+            {
+                double a, b;
+                a = (y1 - y2) / (x1 - x2);
+                b = y2 - a * x2;
+                return (a, b);
+            }
+        }
+    }
+}
