1b75ffc561650ff9de77c9deee423b94d7c98747
Yanzhit <yanzhit@gmail.com>
2021-03-08 02:30:30 +0300

    Add Class OneArray

---
+++ HomeWork1/OneArray.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork1
+{
+    public static class OneArray
+    {
+        // 1. Найти минимальный элемент массива
+        public static int FindMinArray(int[] a)
+        {
+            int  min = a[0];
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (min > a[i])
+                {
+                    min = a[i];
+                }
+            }
+            return min;
+        }
+
+        // 2. Найти максимальный элемент массива
+        public static int FindMaxArray(int[] a)
+        {
+            int max = a[0];
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (max < a[i])
+                {
+                    max = a[i];
+                }
+            }
+            return max;
+        }
+
+        // 3. Найти индекс минимального элемента массива
+        public static int FindIndMinArray(int[] a)
+        {
+            int min = a[0], indMin = 0;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (min > a[i])
+                {
+                    min = a[i];
+                    indMin = i;
+                }
+            }
+            return indMin;
+        }
+
+        // 4. Найти индекс максимального элемента массива
+        public static int FindIndMaxArray(int[] a)
+        {
+            int max = a[0], indMax = 0;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (max < a[i])
+                {
+                    max = a[i];
+                    indMax = i;
+                }
+            }
+            return indMax;
+        }
+
+        // 5. Посчитать сумму элементов массива с нечетными индексами
+        public static int FindSumArrayOddInd(int[] a)
+        {
+            int sum = 0;
+            for (int i = 1; i < a.Length; i += 2)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+
+        // 6. Сделать реверс массива (массив в обратном направлении)
+        public static int[] ReverseArray(int[] a)
+        {
+            int tmp, n= a.Length; 
+            for (int i = 0; i < n / 2; i++)
+            {
+                tmp = a[i];
+                a[i] = a[n - i - 1];
+                a[n - i - 1] = tmp;
+            }
+            return a;
+        }
+
+        // 7. Посчитать количество нечетных элементов массива
+        public static int CountOddArray(int[] a)
+        {
+            int k = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 != 0)
+                {
+                    k += 1;
+                }
+            }
+            return k;
+        }
+
+        // 8. Поменять местами первую и вторую половину массива, 
+        //    например, для массива 1 2 3 4, результат 3 4 1 2,  или для 12345 - 45312.
+        public static int[] SwapHalfArray(int[] a)
+        {
+            int tmp, n= a.Length;
+            for (int i = 0; i != (n / 2); i++)
+            {
+                if (n % 2 == 0)
+                {
+                    tmp = a[i + n / 2];
+                    a[i + n / 2] = a[i];
+                    a[i] = tmp;
+                }
+                else
+                {
+                    tmp = a[i + n / 2 + 1];
+                    a[i + n / 2 + 1] = a[i];
+                    a[i] = tmp;
+                }
+            }
+            return a;
+        }
+
+        // 9. Отсортировать массив по возрастанию одним из способов:  
+        //    пузырьком(Bubble), выбором (Select) или вставками (Insert)) 
+        public static int[] SortArrayAscending(int[] array)
+        {
+            int tmp;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        tmp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        // 10. Отсортировать массив по убыванию одним из способов, (отличным от способа в 9-м задании) :  
+        //     пузырьком(Bubble), выбором (Select) или вставками (Insert))
+        public static int[] SortArrayDescending(int[] array)
+        {
+            int tmp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int indexOfMax = i;
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[indexOfMax] < array[j])
+                    {
+                        indexOfMax = j;
+                    }
+                }
+                tmp = array[i];
+                array[i] = array[indexOfMax];
+                array[indexOfMax] = tmp;
+            }
+            return array;
+        }
+    }
+}
--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -6,45 +6,82 @@ namespace HomeWork1
     {
         static void Main(string[] args)
         {
-          /*  double a = 2, b = 5, c=-7, x1=0, y1=-5, x2=4, y2=3;
-            int n=89;
-            string s1 = "123", s2 = "456";
-            
-            //Переменные
-            Console.WriteLine("  Переменные");
-            Console.WriteLine("1. " + Peremennyye.SolutionEquation(a, b));
-            var s3 = Peremennyye.ToSwap(s1, s2);
-            Console.WriteLine($"2. {s3.Item1} {s3.Item2}");
-            Console.WriteLine("3. " + Peremennyye.Division(a, b));
-            Console.WriteLine("4. " + Peremennyye.SolutionLinearEquation(a, b, c));
-            var ab = Peremennyye.SolutionEquationСoordinates(x1, y1, x2, y2);
-            Console.WriteLine($"5. {ab.Item1} {ab.Item2} ");
-            Console.WriteLine();
+            /*  double a = 2, b = 5, c=-7, x1=0, y1=-5, x2=4, y2=3;
+              int n=89;
+              string s1 = "123", s2 = "456";
 
-            //Ветвление
-            Console.WriteLine("  Ветвление");
-            Console.WriteLine("1. " + Vetvleniye.Solution1(a,b));
-            Console.WriteLine("2. " + Vetvleniye.DefineQuarter(x1,y1));
-            Console.WriteLine("3. " + Vetvleniye.OutputAscending(a,b,c));
-            Console.WriteLine("4. " + Vetvleniye.SolutionOfQuadraticEquation(a, b, c));
-            Console.WriteLine("5. " + Vetvleniye.TranslateToWords(n));
-            Console.WriteLine();
+              //Переменные
+              Console.WriteLine("  Переменные");
+              Console.WriteLine("1. " + Peremennyye.SolutionEquation(a, b));
+              var s3 = Peremennyye.ToSwap(s1, s2);
+              Console.WriteLine($"2. {s3.Item1} {s3.Item2}");
+              Console.WriteLine("3. " + Peremennyye.Division(a, b));
+              Console.WriteLine("4. " + Peremennyye.SolutionLinearEquation(a, b, c));
+              var ab = Peremennyye.SolutionEquationСoordinates(x1, y1, x2, y2);
+              Console.WriteLine($"5. {ab.Item1} {ab.Item2} ");
+              Console.WriteLine();
+
+              //Ветвление
+              Console.WriteLine("  Ветвление");
+              Console.WriteLine("1. " + Vetvleniye.Solution1(a,b));
+              Console.WriteLine("2. " + Vetvleniye.DefineQuarter(x1,y1));
+              Console.WriteLine("3. " + Vetvleniye.OutputAscending(a,b,c));
+              Console.WriteLine("4. " + Vetvleniye.SolutionOfQuadraticEquation(a, b, c));
+              Console.WriteLine("5. " + Vetvleniye.TranslateToWords(n));
+              Console.WriteLine();
+
+              //Циклы
+              Console.WriteLine("  Циклы");
+              Console.WriteLine("1. " + Tsikly.Pow(2, 5));
+              Console.WriteLine("2. " + Tsikly.DivA(250));
+              Console.WriteLine("3. " + Tsikly.CountMensheA(10));
+              Console.WriteLine("4. " + Tsikly.MaxDelitelA(100));
+              Console.WriteLine("5. " + Tsikly.SumDiv7(1,35));
+              Console.WriteLine("6. " + Tsikly.FindNFibonachi(13));
+              Console.WriteLine("7. " + Tsikly.FindDivEvklida(840,1440));
+              Console.WriteLine("8. " + Tsikly.FindNBinarySearch(8));
+              Console.WriteLine("9. " + Tsikly.FindCountOddNumbers(123456789));
+              Console.WriteLine("10. " + Tsikly.FindNumberСonversely(12345));
+              Console.WriteLine("11. " + Tsikly.FindNumberSumEvenMoreSumOdd(50));
+              Console.WriteLine("12. " + Tsikly.FindSameNumbers(500, 99));
+              Console.WriteLine();
+              */
+            // Одномерные массивы
+            Console.WriteLine("  Одномерные массивы");
+            int n = 10;
+            int[] a = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = random.Next(1, n);
+                Console.Write(a[i] + " ");
+            }
+           
+            Console.WriteLine(); /*
+            Console.WriteLine("1. Минимальный элемент массива: " + OneArray.FindMinArray(a));
+            Console.WriteLine("2. Максимальный элемент массива: " + OneArray.FindMaxArray(a));
+            Console.WriteLine("3. Индекс минимального элемента массива: " + OneArray.FindIndMinArray(a));
+            Console.WriteLine("4. Индекс максимального элемента массива: " + OneArray.FindIndMaxArray(a));
+            Console.WriteLine("5. Cумма элементов массива с нечетными индексами: " + OneArray.FindSumArrayOddInd(a));
             */
-            //Циклы
-            Console.WriteLine("  Циклы");
-            Console.WriteLine("1. " + Tsikly.Pow(2, 5));
-            Console.WriteLine("2. " + Tsikly.DivA(250));
-            Console.WriteLine("3. " + Tsikly.CountMensheA(10));
-            Console.WriteLine("4. " + Tsikly.MaxDelitelA(100));
-            Console.WriteLine("5. " + Tsikly.SumDiv7(1,35));
-            Console.WriteLine("6. " + Tsikly.FindNFibonachi(13));
-            Console.WriteLine("7. " + Tsikly.FindDivEvklida(840,1440));
-            Console.WriteLine("8. " + Tsikly.FindNBinarySearch(8));
-            Console.WriteLine("9. " + Tsikly.FindCountOddNumbers(123456789));
-            Console.WriteLine("10. " + Tsikly.FindNumberСonversely(12345));
-            Console.WriteLine("11. " + Tsikly.FindNumberSumEvenMoreSumOdd(50));
-            Console.WriteLine("12. " + Tsikly.FindSameNumbers(500, 99));
+            //Console.WriteLine("6. Реверс массива: ");
+            //OneArray.ReverseArray(a);
 
+            Console.WriteLine("7. Количество нечетных элементов массива: " + OneArray.CountOddArray(a));
+
+            //Console.WriteLine("8. Поменять местами первую и вторую половину массива: ");           
+            //OneArray.SwapHalfArray(a);
+
+            //Console.WriteLine("9. Отсортировать массив по возрастанию : ");
+            //OneArray.SortArrayAscending(a);
+
+            Console.WriteLine("10. Отсортировать массив по убыванию : ");
+            OneArray.SortArrayDescending(a);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
